e743c055eb0efa9e8f05a1a3c34cf95dce3c6adf
Ruhtra <kawanarthurskate@gmail.com>
2024-04-04 14:55:24 -0300

    Adicionado um carrosel com swiper

--- src/routes/Index/IndexRoute.tsx
+++ src/routes/Index/IndexRoute.tsx
@@ -2,6 +2,20 @@ import { Loading } from "../../components/Loading/Loading";
 import { useCarousel } from "../../services/Querys/HomePage";
 import { useRecents } from "../../services/Querys/Profiles";
 
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './styles.css'
+
+// import required modules
+import { EffectCoverflow, Keyboard, Navigation, Pagination } from 'swiper/modules';
 
 export function Index() {
     const {data, isLoading} = useCarousel(true)
@@ -10,6 +24,60 @@ export function Index() {
 
     return (
         <>
+          <Swiper
+            effect={'coverflow'}
+            grabCursor={true}
+            loop={true}
+            centeredSlides={true}
+            slidesPerView={'auto'}
+            coverflowEffect={{
+                rotate: 40,
+                stretch: 40,
+                depth: 100,
+                modifier: .8,
+                scale: 0.8,
+                slideShadows: true,
+            }}
+            // spaceBetween={-20}
+            pagination={true}
+            navigation={true}
+            keyboard={{
+                enabled: true,
+              }}
+            modules={[Keyboard, EffectCoverflow, Pagination, Navigation]}
+            className="mySwiper"
+            
+        >
+            <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+            <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+            </SwiperSlide>
+        </Swiper>
+
+
         <h1>Carousel</h1>
             {isLoading && <Loading />}
              {
