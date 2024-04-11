f6964891a84ec8452d6c6dd74913f1bb904b3720
Ruhtra <kawanarthurskate@gmail.com>
2024-04-11 16:18:39 -0300

    finalizando

--- src/routes/Index/IndexRoute.tsx
+++ src/routes/Index/IndexRoute.tsx
@@ -77,17 +77,21 @@ export function Index() {
             <div className="bloco">
                 <div className="img">
                 </div>
-                    <div className="texto"></div>
+                    <div className="texto">
+                    <div className="msg">
+                    Oi, meu nome é Ramon Sanmir, tenho 34 anos e moro em Fortaleza, desde que eu era muito novo já gostava do meio artistico e internet no geral, com 12 anos fiz um canal no YouTube com amigos e depois comecei a gravar vídeos para as redes sociais, no tempo sem muito apoio e por ser novo acabei sem dar continuidade. Já vendi dindim alcoólico na praia para conseguir comprar meu computador e iniciar a vida na internet, tive alguns planos atrasados pela pandemia e comecei a fazer transmissões na twitch, após esse período tendo como única fonte de renda os jogos e streams, comecei a fazer conteúdo de experiências e estou me reiventando a cada dia.
+                    </div>
+                    </div>
             </div>
 
             <h1>Recents</h1>
 
-            {isLoadingRecents && <Loading />}
+            {/* {isLoadingRecents && <Loading />}
             {
                 dataRecents?.map(e => {
                     return <h1 key={e._id}>{e._id}</h1>
                 })
-            }
+            } */}
         </>
     )
 }
